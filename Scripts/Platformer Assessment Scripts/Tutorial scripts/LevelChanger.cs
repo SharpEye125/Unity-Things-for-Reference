2acba970d67d5a2dbeb3007100a5f5cc2cc4f130
SharpEye125 <51964791+SharpEye125@users.noreply.github.com>
2020-01-22 13:48:00 -0800

    Add files via upload

---
+++ Scripts/Platformer Assessment Scripts/Tutorial scripts/LevelChanger.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LevelChanger : MonoBehaviour
+{
+    public Animator animator;
+    private int levelToLoad;
+    public int lastLevel;
+    // Update is called once per frame
+    private void Start()
+    {
+        lastLevel = GameObject.Find("Player").GetComponent<Player>().level;
+    }
+    void Update()
+    {
+        //if (Input.GetButtonDown("Fire1"))
+        {
+            //FadeToNextLevel();
+
+        }
+    }
+    public void FadeToLevel(int levelIndex)
+    {
+        levelToLoad = levelIndex;
+        animator.SetTrigger("FadeOut");
+    }
+    public void OnFadeComplete()
+    {
+        if (levelToLoad >= 4)
+        {
+            levelToLoad = 0;
+        }
+            SceneManager.LoadScene(levelToLoad);
+    }
+    public void FadeToNextLevel()
+    {
+        FadeToLevel(SceneManager.GetActiveScene().buildIndex + 1);
+        return;
+    }
+    public void FadeToLastLevel(int buildIndex)
+    {
+        lastLevel = GameObject.Find("Player").GetComponent<Player>().level;
+        if (lastLevel >= 3)
+        {
+            lastLevel = 2;
+        }
+        FadeToLevel(levelToLoad = lastLevel);
+        switch (lastLevel)
+        {
+            case 0:
+                FadeToLevel(SceneManager.GetActiveScene().buildIndex + 1);
+                break;
+            //case 1:
+                //FadeToLevel(SceneManager.GetActiveScene().buildIndex + 1);
+                //break;
+            //case 2:
+                //FadeToLevel(SceneManager.GetActiveScene().buildIndex + 2);
+                //break;
+            case 3:
+                FadeToLevel(SceneManager.GetActiveScene().buildIndex + 2);
+                break;
+        }
+        return;
+    }
+}
