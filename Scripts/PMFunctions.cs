b74cc85d94e597f54c858c40bb0ee24833d7a3e2
SharpEye125 <51964791+SharpEye125@users.noreply.github.com>
2020-01-22 13:46:47 -0800

    Add files via upload

---
+++ Scripts/PMFunctions.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PMFunctions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SimpleFunction();
+        int damage = DamageCalc(42, 12);
+        
+    }
+    
+    void SimpleFunction()
+    {
+        Debug.Log("Hello there good sir!");
+    }
+
+    public int DamageCalc(int damage, int armor)
+    {
+        Debug.Log("You deal" + (damage - armor) + "damage");
+        return damage - armor;
+    }
+
+    public int DamageCalc(int damage, int armor, bool crit)
+    {
+        Debug.Log("You deal" + (damage - armor) + "damage");
+        return damage - armor;
+    }
+
+}
---
+++ Scripts/ParallaxGrid.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxGrid : MonoBehaviour
+{
+    Transform camera;
+    Vector3 previousCamPos;
+    public float parallaxScale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = Camera.main.transform;
+        previousCamPos = camera.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float parallax = (previousCamPos.x - camera.position.x) * parallaxScale;
+        Vector3 pos = transform.position;
+        pos.x += parallax;
+        transform.position = pos;
+        previousCamPos = camera.position;
+    }
+}
